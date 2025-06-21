af60565a47acf2722e098fde929de51cfb334272
Kornphisit Sriwiset <kornphisit31@outlook.com>
2025-06-22 03:21:50 +0700

    befor ex10

---
+++ react_app/src/components/Header.jsx
@@ -0,0 +1,6 @@
+function Header() {
+  return(
+    <h1>แอพของฉัน</h1>
+  );
+}
+export default Header;
---
+++ react_app/src/components/PersonList.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function PersonList() {
+  const [data, setData] = useState([
+    { id: 1, name: "Korn", gender: "ชาย" },
+    { id: 2, name: "Nim", gender: "หญิง" },
+    { id: 3, name: "John Doe", gender: "ชาย" },
+  ]);
+  const [show, setShow] = useState(true);
+  return (
+    <>
+      <h1>จำนวนประชากร {data.length} คน</h1>
+      <button onClick={() => setShow(!show)}>{show ? "ซ่อน" : "แสดง"}</button>
+      <u>
+        {show &&
+          data.map((item) => (
+            <li key={item.id}>
+              {" "}
+              <h3>
+                {item.id}| {item.name}| {item.gender}
+              </h3>
+            </li>
+          ))}
+      </u>
+    </>
+  );
+}
+export default PersonList;
