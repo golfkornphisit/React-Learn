3c86019f40235269f6b622e140cf0afaa6dadda7
Kornphisit Sriwiset <kornphisit31@outlook.com>
2025-06-22 09:24:35 +0700

    befor ex20

--- react_app/src/components/AddForm.jsx
+++ react_app/src/components/AddForm.jsx
@@ -1,12 +1,15 @@
 import "./AddForm.css"
+import { useState } from "react";
 
 export default function AddForm() {
+    const [name, setName] = useState("");
+    const [gender, setGender] = useState("");
     return (
         <section className="container">
             <form action="">
                 <label htmlFor="">ชื่อประชากร</label>
-                <input type="text" />
-                <select name="" id="">
+                <input type="text" value={name}onChange={(e)=>setName(e.target.value)}/>
+                <select name="" id="" value={gender} onChange={(e)=>setGender(e.target.value)}>
                     <option value="ชาย">ชาย</option>
                     <option value="หญิง">หญิง</option>
                 </select>
