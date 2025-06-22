10c2d0982947fb74872344c2a9ce090d463e6636
Kornphisit Sriwiset <kornphisit31@outlook.com>
2025-06-22 08:15:07 +0700

    befor ex16

--- react_app/src/components/Header.jsx
+++ react_app/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import "./Header.css";
-function Header() {
+function Header(props) {
+  // Destructuring props to get title
+  const { title } = props;
   return (
     <nav>
-      <h1>My App</h1>
+      <h1>{title}</h1>
       <button>Ligth/Dark</button>
     </nav>    
   );
